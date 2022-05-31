07f8c4dea3ea423e5e21c9c337795fa89e4565f6
ChristofferBillman <christoffer.billman@gmail.com>
2022-06-01 00:01:34 +0200

    feat: added GreetUp post

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,80 +1,108 @@
-export const posts = [{
-	"name": "labplant",
-	"title": "Lab Plant",
-	"body": "Detta var slutprojektet i kursen Design och Tjänsteutveckling för IoT vid Umeå Universitet. Kursen handlade till stor del om att bygga egna system och experimentera med mikrokontrollers och sensorer. I projektet byggde jag tillsammans med tre andra ett system för att övervaka och vattna krukväxter med hjälp av en ESP-32, ett urval sensorer och en Raspberry Pi 4.",
-	"images": [
-		"img/lab-plant/setup.jpeg",
-		"img/lab-plant/readme_img.png",
-		"img/lab-plant/soilsensor.jpeg",
-	],
-	"imagePosition": "right",
-	"order": "1",
-	"ghlink": "https://github.com/ChristofferBillman/ProjektIoT",
-	"tags": [
-		"IoT",
-		"NodeJS",
-		"Web"
-	]
-}, {
-	"name": "portfolio3",
-	"title": "Portfolio v.3",
-	"body": "Den tredje försöket till en portfoliosida! Denna version kom faktiskt till, och projektet blev mycket mer ambitiöst än vad de tidigare två var! Två flugor en smäll!",
-	"images": [
-		"img/portfolio3/grafisk-profil-rosa.webp",
-		"img/portfolio3/grafisk-profil.webp",
-		"img/portfolio3/preview.webp"
-	],
-	"imagePosition": "right",
-	"order": "2",
-	"ghlink": "https://github.com/ChristofferBillman/Portfolio3-Deployed"
-}, {
-	"name": "skissteknik",
-	"title": "Skissteknik - Rumbokningsystem",
-	"body": "Denna uppgift var del av kursen 'Design för användarupplevelse' hos designhögskolan i Umeå. Det gick huvudsakligen ut på att lära sig hur man arbetar med wireframes och iterativa designprocesser",
-	"images": [
-		"img/skissteknik/m1.webp",
-		"img/skissteknik/m2.webp",
-		"img/skissteknik/m3.webp",
-		"img/skissteknik/m4.webp",
-		"img/skissteknik/m5.webp",
-		"img/skissteknik/m6.webp",
-		"img/skissteknik/m7.webp",
-		"img/skissteknik/m8.webp"
-	],
-	"imagePosition": "top",
-	"order": "3"
-}, {
-	"name": "portfolio2",
-	"title": "Portfolio v.2",
-	"body": "Andra försöket till en portfoliosida - gick sådär. Blev aldrig riktigt nöjd med designen så det projektet kom som längst med Hi-Fi prototyper i Adobe XD.",
-	"images": [
-		"img/portfolio2/cards.webp",
-		"img/portfolio2/desktop.webp",
-		"img/portfolio2/mobile.webp",
-		"img/portfolio2/menu.webp"
-	],
-	"imagePosition": "top",
-	"order": "4"
-}, {
-	"name": "portfolio1",
-	"title": "Portfolio v.1",
-	"body": "Första försöket! Inte så mycket att säga här förutom att jag kom ännu kortare än vid andra försöket.",
-	"images": [
-		"img/portfolio1/preview.webp"
-	],
-	"imagePosition": "top",
-	"order": "5"
-}, {
-	"name": "vbost",
-	"title": "Västerbottensost -  varumärkeshem",
-	"body": "Slutprojekt i kursen design för användarupplevelse. Vi fick 4 veckor på oss att arbeta fram ett så kallat varumärkeshem till Västerbottensost.",
-	"images": [
-		"img/vbost/ostprovning.webp",
-		"img/vbost/overview.webp",
-		"img/vbost/desktop.webp",
-		"img/vbost/desktop2.webp",
-		"img/vbost/mobile.webp"
-	],
-	"imagePosition": "right",
-	"order": "6"
-}]
+export const posts = [
+	{
+		"name": "greetup",
+		"title": "GreetUp",
+		"subtitle": "Ett projekt",
+		"body": "Slutprojekt i kurserna Teknik för Sociala Medier samt Applikationsutveckling för Internet. Vi fick 5 veckor på oss att utveckla ett socialt media. Resultatet blev GreetUp, ett socialt media anpassat för äldre i Australien med funktionsvariationer.",
+		"images": [
+			"img/greetup/login-zoomedout.png",
+		],
+		"imagePosition": "right",
+		"order": "1",
+		"ghlink": "https://github.com/ChristofferBillman/ProjektIoT",
+		"tags": [
+			"React",
+			"API",
+			"C#",
+			".NET Core",
+		],
+		"year": "2022",
+	}, {
+		"name": "labplant",
+		"title": "Lab Plant",
+		"subtitle": "Ett projekt",
+		"body": "Detta var slutprojektet i kursen Design och Tjänsteutveckling för IoT vid Umeå Universitet. Kursen handlade till stor del om att bygga egna system och experimentera med mikrokontrollers och sensorer. I projektet byggde jag tillsammans med tre andra ett system för att övervaka och vattna krukväxter med hjälp av en ESP-32, ett urval sensorer och en Raspberry Pi 4.",
+		"images": [
+			"img/lab-plant/setup.jpeg",
+			"img/lab-plant/readme_img.png",
+			"img/lab-plant/soilsensor.jpeg",
+		],
+		"imagePosition": "right",
+		"order": "1",
+		"ghlink": "https://github.com/ChristofferBillman/ProjektIoT",
+		"tags": [
+			"IoT",
+			"Raspberry Pi",
+			"NodeJS",
+			"Web"
+		],
+		"year": "2022",
+	}, {
+		"name": "portfolio3",
+		"title": "Portfolio v.3",
+		"body": "Den tredje försöket till en portfoliosida! Denna version kom faktiskt till, och projektet blev mycket mer ambitiöst än vad de tidigare två var! Två flugor en smäll!",
+		"images": [
+			"img/portfolio3/grafisk-profil-rosa.webp",
+			"img/portfolio3/grafisk-profil.webp",
+			"img/portfolio3/preview.webp"
+		],
+		"imagePosition": "right",
+		"order": "2",
+		"ghlink": "https://github.com/ChristofferBillman/Portfolio3-Deployed",
+		"year": "2021"
+	}, {
+		"name": "skissteknik",
+		"title": "Rumbokningssystem",
+		"subtitle": "Skissteknik",
+		"body": "Denna uppgift var del av kursen 'Design för användarupplevelse' hos designhögskolan i Umeå. Det gick huvudsakligen ut på att lära sig hur man arbetar med wireframes och iterativa designprocesser",
+		"images": [
+			"img/skissteknik/m1.webp",
+			"img/skissteknik/m2.webp",
+			"img/skissteknik/m3.webp",
+			"img/skissteknik/m4.webp",
+			"img/skissteknik/m5.webp",
+			"img/skissteknik/m6.webp",
+			"img/skissteknik/m7.webp",
+			"img/skissteknik/m8.webp"
+		],
+		"imagePosition": "top",
+		"order": "3",
+		"year": "2021",
+	}, {
+		"name": "portfolio2",
+		"title": "Portfolio v.2",
+		"body": "Andra försöket till en portfoliosida - gick sådär. Blev aldrig riktigt nöjd med designen så det projektet kom som längst med Hi-Fi prototyper i Adobe XD.",
+		"images": [
+			"img/portfolio2/cards.webp",
+			"img/portfolio2/desktop.webp",
+			"img/portfolio2/mobile.webp",
+			"img/portfolio2/menu.webp"
+		],
+		"imagePosition": "top",
+		"order": "4",
+		"year": "2020",
+	}, {
+		"name": "portfolio1",
+		"title": "Portfolio v.1",
+		"body": "Första försöket! Inte så mycket att säga här förutom att jag kom ännu kortare än vid andra försöket.",
+		"images": [
+			"img/portfolio1/preview.webp"
+		],
+		"imagePosition": "top",
+		"order": "5"
+	}, {
+		"name": "vbost",
+		"title": "Västerbottensost",
+		"subtitle": "Ett varumärkeshem",
+		"body": "Slutprojekt i kursen design för användarupplevelse. Vi fick 4 veckor på oss att arbeta fram ett så kallat varumärkeshem till Västerbottensost.",
+		"images": [
+			"img/vbost/ostprovning.webp",
+			"img/vbost/overview.webp",
+			"img/vbost/desktop.webp",
+			"img/vbost/desktop2.webp",
+			"img/vbost/mobile.webp"
+		],
+		"imagePosition": "right",
+		"order": "6",
+		"year": "2021"
+	}]
