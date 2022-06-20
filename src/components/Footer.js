95a53adc358f4b7f87ccdc6735e7090b4a267bb5
ChristofferBillman <christoffer.billman@gmail.com>
2022-06-20 14:45:41 +0200

    fix: footer now working on mobile as well

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import Page from './Page'
+import DualColumnPage from './DualColumnPage'
 import { scrollToElement } from '../utils/util';
 
 import '../styles/Footer.css'
@@ -8,30 +8,35 @@ import '../styles/Footer.css'
  */
 
 export default function Footer() {
-	return (
-		<Page contentSide='left' style={{ background: 'var(--black)' }} id='footer'>
-			<div className='footer-content'>
-				<div>
-					<h1 className='white'>Nu tog det slut</h1>
-					<p className='clickable-text' onClick={() => scrollToElement('landing')}>Till toppen ^</p>
-				</div>
 
-				<div className='contact-section'>
-					<h1 className='white'>Kontakt</h1>
-					<div className='contact'>
-						<h4 className='white nomargin'>Mejl</h4>
-						<p className='white nomargin'>christofferbillman@gmail.com</p>
-					</div>
-					<div className='contact'>
-						<h4 className='white nomargin'>Telefon</h4>
-						<p className='white nomargin'>070 577 22 97</p>
-					</div>
-					<div className='contact'>
-						<h4 className='white nomargin'>LinkedIn</h4>
-						<p className='white nomargin'>Christoffer Billman</p>
-					</div>
+	const content =
+		<div className='footer-content'>
+			<div>
+				<h1 className='white'>Nu tog det slut</h1>
+				<p className='clickable-text' onClick={() => scrollToElement('landing')}>Till toppen ^</p>
+			</div>
+
+			<div className='contact-section'>
+				<h1 className='white'>Kontakt</h1>
+				<div className='contact'>
+					<h4 className='white nomargin'>Mejl</h4>
+					<p className='white nomargin'>christofferbillman@gmail.com</p>
+				</div>
+				<div className='contact'>
+					<h4 className='white nomargin'>Telefon</h4>
+					<p className='white nomargin'>070 577 22 97</p>
+				</div>
+				<div className='contact'>
+					<h4 className='white nomargin'>LinkedIn</h4>
+					<p className='white nomargin'>Christoffer Billman</p>
 				</div>
 			</div>
-		</Page>
+		</div>
+	return (
+		<DualColumnPage
+			style={{ background: 'var(--black)' }}
+			id='footer'
+			leftContent={content}
+		/>
 	);
 }
