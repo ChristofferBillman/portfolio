02522202aee7351513e2c0e0b9765a9562388038
ChristofferBillman <christoffer.billman@gmail.com>
2022-09-20 22:37:59 +0200

    fix: very hacky fix for mobile landing banner. + removed warning text on mobile devices

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -11,21 +11,12 @@ import { isMobile } from '../utils/util'
 export default function Landing() {
 	return (
 		<div>
-			<Page img='landing-img.png' navigationButtons={false} id='landing' useGallery={false} contentSide='left'>
+			<Page img={['landing-img.png']} navigationButtons={false} id='landing' useGallery={true} contentSide='left'>
 				<div className='inner-content-container'>
 
 					<h1 className='fadeIn stagger-1'>Christoffer <br />Billman</h1>
 					<h3 className='fadeIn stagger-2'>Civilingengörsstudent</h3>
-					{isMobile() ? (
-						<div className='fadeIn stagger-2'>
-							<p className='justify-text' style={{ color: '#d48f22', fontWeight: 700 }}>
-								Det verkar som om att du besöker webbplatsen från en mobiltelefon.<br /><br />
-								Mobilversionen är under konstruktion och kanske inte fungerar som den ska.
-							</p>
-						</div>
-					) : (
-						null
-					)}
+
 					<h4 className='justify-text fadeIn stagger-3'>
 						Studerar Civingenjör Interaktion och Design vid Umeå Universitet.
 						Grafisk design, programmering och webbutveckling är saker som intresserar!
