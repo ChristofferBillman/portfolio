ca2645bb0f600fcee6b543eceaada823f05c2504
Christoffer Billman <christoffer.billman@gmail.comm>
2024-08-01 12:13:26 +0200

    content: updated phrasing in about section.

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -96,7 +96,7 @@ const translations: Language[] = [
 		AboutP1: 'Jag är Christoffer! Kul att du besöker min portfolio!',
 		AboutP2: 'I våras tog jag examen från civilingenjörsprogrammet interaktion och design vid Umeå universitet. Under mina år på programmet har jag studerat allt från programmering och matematik, till UX och psykologi. Programmet har gett mig en bred och unik kompetensprofil. Jag har god förståelse för hela designprocessen från idé till implementation. Verktyg och processer så som double diamond, design thinking och UX honeycomb är välbekanta.',
 		AboutP3: 'Jag har även god förståelse för det tekniska, och har praktisk erfarenhet av backend såväl som frontend, men har även kunskap om de teoretiska grunderna inom datavetenskapen.',
-		AboutBold: 'Med andra ord, så är jag expert på mötet mellan människa och maskin - en kombination som är både unik och oerhört värdefull.',
+		AboutBold: 'Med andra ord, så har jag stor erfarenhet av samspelet mellan människa och maskin - en kombination som är både unik och värdefull.',
 		AboutP5: 'På en mer personlig not, så är jag uppvuxen i norrlandsfjällen ungefär 3 mil utanför skidorten Hemavan. Med jämna mellanrum så hälsar jag på min familj där uppe, kanske tar en dag i skidbacken eller en dagstur upp på fjället med skotern. När jag väl är hemma i Umeå, så brukar jag på min fritid umgås med vänner, spela datorspel (särskilt minecraft ⛏) och löpträna.',
 		AboutP6: 'Fortsätter du skrolla ned så får du se ett axplock av de projekt som jag arbetat med. Har du frågor eller funderingar, hör gärna av dig!'
 	},
@@ -118,7 +118,7 @@ const translations: Language[] = [
 		AboutP1: 'I am Christoffer! How fun that you are visiting my portfolio!',
 		AboutP2: 'This spring, I graduated from the program M.Sc in Interaction and Design at Umeå University. During my years in the program I have studied everything from programming and mathematics, to UX and psychology. My education has given me a broad and unique competence profile. I have a good understanding of the whole design process, from idea to implementation. Tools and processes like Double Diamond, Design Thinking and UX honeycomb are very familiar.',
 		AboutP3: 'I also have a good understanding of the technical side of things, where I have practical experience of backend as well as frontend development, but also the theoretical foundations of computer science.',
-		AboutBold: 'In other words, I am an expert of the intersection between human and machine - a combination that is both unique and incredibly valuable.',
+		AboutBold: 'In other words, I have extensive insights into the intersection between human and machine - a combination that is both unique and valuable.',
 		AboutP5: 'On a more personal note, I grew up in the sparsely populated mountain region in northern Sweden. Every now and then I pay a visit to my family who live there, and go downhill skiing or go on a snowmobile ride up on the mountains. When I am at home in Umeå, I usually hang out with friends, play videogames (especially Minecraft ⛏) and run.',
 		AboutP6: 'If you keep scrolling, an assortment of projects I have worked with are showcased. If you have any questions, feel free to contact me!'
 	},
